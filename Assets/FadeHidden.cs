b5aed869acd10c0c3047be0cd0ab02d664c5ce71
fastsystem <info@fast-system.jp>
2019-03-17 19:44:54 +0900

    init

---
+++ Assets/FadeHidden.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeHidden : MonoBehaviour
+{
+    public float startDistance = 10;
+    public float hiddenDisanta = 2;
+
+    void Update()
+    {
+        var d = Vector3.Distance(Camera.main.transform.position, transform.position);
+
+        var color = this.GetComponent<Renderer>().material.color;
+        if (d <= hiddenDisanta)
+            color.a = 0.0f;
+        else if (d <= startDistance)
+            color.a = (d - hiddenDisanta) / (startDistance - hiddenDisanta);
+        else
+            color.a = 1.0f;
+        this.GetComponent<Renderer>().material.color = color;
+    }
+}
